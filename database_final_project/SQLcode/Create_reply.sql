f444f7b57df8a3384d55f9cd504c89cad1a10958
Chuan-Yu Wu <patrick050292@gmail.com>
2023-04-02 19:22:30 +0800

    Update Create_reply.sql

--- database_final_project/SQLcode/Create_reply.sql
+++ database_final_project/SQLcode/Create_reply.sql
@@ -41,11 +41,11 @@ create table mbti_match
 copy reply(index, email, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, 
 			q11, q12, q13, q14, q15, q16, school_name, gender, study_group,
 		   star_sign, age, area, mbti_result)
-from 'D:\111-1\Data_Base\Final_project\Reply.csv'
+from 'database_final_project\data\Reply.csv'
 delimiter ','
 csv header;
 
 copy mbti_match(asked_mbti, suitable_mbti)
-from 'D:\111-1\Data_Base\Final_project\MBTI_match.csv'
+from 'database_final_project\data\MBTI_match.csv'
 delimiter ','
 csv header;
